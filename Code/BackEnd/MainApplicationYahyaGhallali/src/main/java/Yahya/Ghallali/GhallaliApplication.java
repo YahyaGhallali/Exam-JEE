d8acd2e7fd50accb24ab93c4bb50b0cb5ba878a8
Yahya Ghallali <Yahyaghallali88@gmail.com>
2025-05-19 08:59:58 +0100

    main spring project

---
+++ Code/BackEnd/MainApplicationYahyaGhallali/src/main/java/Yahya/Ghallali/GhallaliApplication.java
@@ -0,0 +1,13 @@
+package Yahya.Ghallali;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GhallaliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GhallaliApplication.class, args);
+	}
+
+}
